07e0b6be4a5416f5455901bf9b170e0f1fae9e86
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-01-15 19:08:13 +0200

    salesTax exercise in java

--- labs/salesTax/__init__.py
+++ labs/salesTax/__init__.py
@@ -2,19 +2,37 @@ import check50
 import check50.c
 
 @check50.check()
-def exists():
+def Cexists():
     """salesTax.cpp exists."""
     check50.exists("salesTax.cpp")
 
-@check50.check(exists)
-def compiled():
-   """salesTax executable is generated"""
+@check50.check(Cexists)
+def Ccompiled():
+   """C++ salesTax executable is generated"""
    check50.exists("salesTax")
 
 
-@check50.check(compiled)
-def produces_correct_output():
-    """produces correct output for sample input"""
+@check50.check(Ccompiled)
+def Cproduces_correct_output():
+    """C++ code produces correct output for sample input"""
     check50.run(
         "./salesTax ").stdin("100").stdin(".30").stdin("0.24").stdout("100 0.3 0.24 130 31.2 161.2").exit(0)
 
+
+@check50.check()
+def Jexists():
+    """salesTax.java exists."""
+    check50.exists("salesTax.java")
+
+
+@check50.check(Jexists)
+def Jcompiled():
+   """salesTax java executable is generated"""
+   check50.exists("salesTax.class")
+
+
+@check50.check(Jcompiled)
+def Jproduces_correct_output():
+    """Java code produces correct output for sample input"""
+    check50.run(
+        "javac salesTax ").stdin("100").stdin(".30").stdin("0.24").stdout("100 0.3 0.24 130 31.2 161.2").exit(0)
---
+++ labs/salesTax/submit.sh
@@ -0,0 +1,1 @@
+submit50 mkotsovoulou/itc2197sp22/main/labs/salesTax
